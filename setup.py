0a20ae2868db51c452c87972c9c445041d532d4f
Mostafa Mosavi <mostafa.uwsgi@gmail.com>
2022-08-10 17:21:23 +0430

    :bookmark:  updated to 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="protolizer",
-    version="1.0.1",
+    version="1.0.2",
     author='MosyDev',
     author_email='mostafa.uwsgi@gmail.com',
     description='A simple library to serialize and deserialize protobuf messages',
